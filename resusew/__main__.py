94b44661bfb525069282aa0d0e20c46bb63c0fbc
Ben Lee <ben.lee8602@gmail.com>
2026-01-31 01:55:55 -0500

    pipx stuff

--- resusew/__main__.py
+++ resusew/__main__.py
@@ -1,36 +1,5 @@
-import sys
-import resusew
+from resusew.cli import main
 
-def load_required_file(filename: str) -> str:
-    try:
-        with open(filename) as f:
-            return f.read()
-    except Exception as e:
-        print(e)
-        exit(1)
-
-if len(sys.argv) != 4:
-    print(
-        "usage: py -m resusew "
-        "resume.xyz.resusew "
-        "jobdesc.txt "
-        "out.xyz"
-    )
-    exit(1)
-
-resume_file: str = sys.argv[1]
-jobdesc_file: str = sys.argv[2]
-out_file: str = sys.argv[3]
-
-resume: str = load_required_file(resume_file)
-jobdesc: str = load_required_file(jobdesc_file)
-
-out: str = resusew.run(resume, jobdesc)
-
-try:
-    with open(out_file, 'w') as f:
-        f.write(out)
-except Exception as e:
-    print(e)
-    exit(1)
+if __name__ == "__main__":
+    main()
 
---
+++ resusew/cli.py
@@ -0,0 +1,37 @@
+import sys
+import resusew
+
+def load_required_file(filename: str) -> str:
+    try:
+        with open(filename) as f:
+            return f.read()
+    except Exception as e:
+        print(e)
+        exit(1)
+
+def main():
+    if len(sys.argv) != 4:
+        print(
+            "usage: resusew "
+            "resume.xyz.resusew "
+            "jobdesc.txt "
+            "out.xyz"
+        )
+        exit(1)
+
+    resume_file: str = sys.argv[1]
+    jobdesc_file: str = sys.argv[2]
+    out_file: str = sys.argv[3]
+
+    resume: str = load_required_file(resume_file)
+    jobdesc: str = load_required_file(jobdesc_file)
+
+    out: str = resusew.run(resume, jobdesc)
+
+    try:
+        with open(out_file, 'w') as f:
+            f.write(out)
+    except Exception as e:
+        print(e)
+        exit(1)
+
